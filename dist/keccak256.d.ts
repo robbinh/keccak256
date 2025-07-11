ccc3cff6347e5d76ea0b3c2cea41aecd7050c678
Josh <josh@capygo.io>
2025-07-11 22:14:11 +0800

    fix for vite

---
+++ dist/keccak256.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+import BN from 'bn.js';
+import { Buffer } from 'buffer';
+declare function keccak256(value: Buffer | BN | string | number): Buffer;
+export = keccak256;
+//# sourceMappingURL=keccak256.d.ts.map
